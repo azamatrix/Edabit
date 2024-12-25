0df64c4857e519583d3f61b0b30842bf06a6738c
RyskulievA <azamat.ryskuliev@gmail.com>
2024-12-25 17:20:35 +0600

    Challenge

---
+++ src/main/java/edabitTasks/Challenge.java
@@ -0,0 +1,29 @@
+package edabitTasks;
+
+import java.util.*;
+
+public class Challenge {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.println(Arrays.toString(reverseArray(arr)));
+    }
+
+    public static int[] reverseArray(int[] arr) {
+        int start = 0, end = arr.length - 1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        return arr;
+    }
+}
